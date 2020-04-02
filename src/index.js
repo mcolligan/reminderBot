77b3627272d09cc08c2d4ea9b54e05933ced1c60
mcolligan <mcolligan1@gmail.com>
2020-04-01 22:42:25 -0600

    Lunch/Dinner testing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const { token } = require('../config.js');
-
+const cron = require('node-cron');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('Ready');
-});
+  console.log(`Bot Start Time: ${new Date()}`);
+  // console.log('--', client.channels)
+  cron.schedule('30 12 * * *', () => {
+    client.channels.cache.get('695103171017834557').send(`Yay. Lunch Time!`);
+  });
 
-client.on('message', message => {
-  console.log(message.content);
-})
+  cron.schedule('30 17 * * *', () => {
+    client.channels.cache.get('695103171017834557').send(`Dinner Time! Enjoy.`);
+  });
+});
 
 client.login(token);

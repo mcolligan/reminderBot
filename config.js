967b32118c6b2461bdbd5c99ee153d38adc025b4
mcolligan <mcolligan1@gmail.com>
2020-04-10 17:53:52 -0600

    Update for stand down

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  token: process.env.KEY
+  token: process.env.KEY,
+  sd: process.env.STAND,
+  rem: process.env.REM,
+  t: process.env.T
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { token } = require('../config.js');
+const { token, sd, rem, t } = require('../config.js');
 const cron = require('node-cron');
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -11,9 +11,23 @@ const today = () => {
 client.once('ready', () => {
   console.log(`Ava Start Time: ${new Date()}`);
 
+  cron.schedule('45 19 * * *', () => {
+    let now = today();
+
+    if (now !== 6 && now !== 0) {
+      client.channels.cache.get(sd).send(`@everyone Let's stand down`);
+    }
+  });
+
+  cron.schedule('15 17 * * *', () => {
+    if (today() === 6) {
+      client.channels.cache.get(sd).send(`@everyone Let's stand down`);
+    }
+  });
+
   cron.schedule('30 12 * * *', () => {
     if (today() !== 0) {
-      client.channels.cache.get('696742553118179348').send(`Yay. Lunch Time!`);
+      client.channels.cache.get(rem).send(`Yay. Lunch Time!`);
     }
   });
 
@@ -21,7 +35,7 @@ client.once('ready', () => {
     let now = today();
 
     if (now !== 6 && now !== 0) {
-      client.channels.cache.get('696742553118179348').send(`Dinner Time! Enjoy.`);
+      client.channels.cache.get(rem).send(`Dinner Time! Enjoy.`);
     }
   });
 });

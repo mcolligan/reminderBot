5edca8c4cd74186e4e5d5f9281571ed1191d3ce7
mcolligan <mcolligan1@gmail.com>
2020-05-27 17:11:59 -0600

    Added jr role for mentions

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ module.exports = {
   sd: process.env.STAND,
   rem: process.env.REM,
   jr: process.env.JR, // currently bld10
+  jrRole: process.env.JR_ROLE_ID,
   cred: process.env.CREDENTIALS_PATH,
   calId: process.env.CALENDAR_ID,
   prLink: process.env.PAIR_FORM_LINK,
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { getCalendarEvents } = require('./googleEvents/googleEvents');
 const cron = require('node-cron');
 const Discord = require('discord.js');
 
-const { token, sd, rem, prLink } = require('../config.js');
+const { token, sd, rem, prLink, jr, jrRole } = require('../config.js');
 const { makeCronString, today } = require('./utils/bot.utils');
 
 const client = new Discord.Client();
@@ -48,25 +48,25 @@ client.once('ready', () => {
       client.channels.cache.get(rem).send(`Dinner Time! Enjoy.`);
     }
   });
-});
-/////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////
 
-//        PAIR FEEDBACK                                     //
-cron.schedule('30 08 * * *', () => {
-  let pairFeedbackTime = '';
-  getCalendarEvents((res) => {
-    if (res) {
-      pairFeedbackTime = makeCronString(res.pairRef);
-      cron.schedule(`${pairFeedbackTime}`, () => {
-        client.channels.cache
-          .get(jr)
-          .send(
-            `@Junior Please fill out the pair reflection form. Thank you and good day -- ${prLink}`
-          );
-      });
-    }
+  //        PAIR FEEDBACK                                     //
+  cron.schedule('30 08 * * *', () => {
+    let pairFeedbackTime = '';
+    getCalendarEvents((res) => {
+      if (res) {
+        pairFeedbackTime = makeCronString(res.pairRef);
+        cron.schedule(`${pairFeedbackTime}`, () => {
+          client.channels.cache
+            .get(jr)
+            .send(
+              `${jrRole} Please fill out the pair reflection form. Thank you and good day -- ${prLink}`
+            );
+        });
+      }
+    });
   });
+  /////////////////////////////////////////////////////////////////
 });
-/////////////////////////////////////////////////////////////////
 
 client.login(token);
